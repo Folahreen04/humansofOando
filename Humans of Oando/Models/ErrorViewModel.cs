c57c7bb791d02fd7c7a067eb7cf54e451c0d902c
Oladunni Folarin <foladunni@oandoenergyresources.com>
2021-07-13 17:32:56 +0100

    First Push

---
+++ Humans of Oando/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Humans_of_Oando.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
